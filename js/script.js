a34c0636e78c92da2986cfda9de036a05f7fd86d
MarcelaLeon <marcelatorresleon15@gmail.com>
2021-07-06 17:29:28 -0500

    [UPDATE] modificacion de media querys

--- js/script.js
+++ js/script.js
@@ -1,154 +1,198 @@
 document.getElementById("timer").innerHTML="00:10";
 
-let habilitado = true;
+let enabled;
+let seconds;
+let countdown;
 
-let seconds=10;
+if (seconds === undefined){
+  seconds=10;
+  countdown =10;
+}
+
 let timer;
-let ganados = 0, empatados = 0, perdidos = 0;
+let won = 0, tied = 0, lost = 0;
 
 
-/*let option = document.querySelectorAll("#option");
-let youOption = document.querySelector("#you-choice");
-let machineOption = document.querySelector("#machine-choice");*/
+let inLocal = JSON.parse(localStorage.getItem("winners"));
 
-
-function myFunction() {
-
-  if(seconds<1){habilitado=false}
-  
-  if(seconds < 10) { // I want it to say 1:00, not 60
-    document.getElementById("timer").innerHTML = seconds; 
-  }
-  if (seconds >0 ) { // so it doesn't go to -1   
-    seconds--;
-  } else {
-    //clearInterval(timer);
-     let winner= document.querySelector(".winner");
-     if(ganados>perdidos){
-      winner.innerHTML += `<p>You</p>`;
-      /*let continuar = confirm("¿Quieres seguir jugando?");
-      if (continuar){
-        document.location.reload();
-      }
-      let cargar = window.setInterval(function() {
-        document.location.reload();
-      }, 1000);*/
-      
-     }
-     else if(perdidos>ganados){
-      winner.innerHTML += `<p>Machine</p>`;
-      /*let cargar = window.setInterval(function() { 
-        document.location.reload;
-      }, 1000);*/
-      
-     }
-     else{
-      winner.innerHTML += `<p>Nobody</p>`;
-      /*let cargar = window.setInterval(function() { 
-        document.location.reload();
-      }, 1000);*/
-     }
-     clearInterval(timer);
-         
-     
-     //document.getElementById("timer").innerHTML="01:00";
-  }
-  
+if(inLocal == null) {
+  inLocal = [];
 }
 
 
 
+let winner= document.querySelector(".winner");
+inLocal.forEach(function(element) {
+  winner.innerHTML += `<p>${element}</p>`;
+})
 
+/**----------------Timekeeper-------------------- */
+function myFunction() {
+    
+  if(seconds < countdown) { 
+    document.getElementById("timer").innerHTML = seconds; 
+  }
 
+  if (seconds >0 ) {   
+    seconds--;
+  } 
+  else {
+    
+      if(won>lost){
+        if(inLocal.length > 6){
+          localStorage.removeItem("winners");
+          inLocal=[];
+        }
+        inLocal.push("You 🥇 ");
+        localStorage.setItem("winners", JSON.stringify(inLocal));
+        alert("You have won this round of the game 😎")
+        location.reload();
+      }
+    else if(lost>won){
+      if(inLocal.length > 6){
+        localStorage.removeItem("winners");
+        inLocal=[];
+      }
+      inLocal.push("Machine 💻");
+        localStorage.setItem("winners", JSON.stringify(inLocal));
+        alert("You have lost this round of the game,\n good luck next time 😥")
+        location.reload();
+       }
+    else{
+      if(inLocal.length > 6){
+        localStorage.removeItem("winners");
+        inLocal=[];
+      }
+      inLocal.push("Nobody");
+        localStorage.setItem("winners", JSON.stringify(inLocal));
+        alert("You have tied this round of the game 🤪")
+        location.reload();
+       }  
+    
+     clearInterval(timer);
+  }
+  
+}
 
-//let button = document.querySelector(".fa-google-play");
-let button = document.querySelector("#play");
-button.addEventListener("click", function(){ //
-     // every second
-     
-     habilitado=true;
-        play();
-      
-      
+/**----------------Click setting------------------- */
+let setting = document.querySelector("#setting");
+let list = document.querySelector("#list");
+setting.addEventListener("click",function(){
+  list.classList.toggle("show");
 })
 
 
+/**----------------Click time------------------- */
+let times = document.querySelectorAll(".time");
+times.forEach(function(time){ 
+  time.addEventListener("click",function(){
+    seconds = time.value;
+    if(seconds == 10){
+      countdown =10;
+      document.getElementById("timer").innerHTML="00:10";
+    }
+    else if(seconds == 30){
+      countdown=30;
+      document.getElementById("timer").innerHTML="00:30";
+    }
+    else if(seconds == 60){
+      countdown=60;
+      document.getElementById("timer").innerHTML="01:00";
+    }
+  })
+})
 
-/**REVIZAR LUEGO --Modo oscuro boton*/
-//capturar el body
+/**----------------Click rules------------------- */
+let rules = document.querySelector("#rules");
+let rule1 = document.querySelector(".rule1");
+let closeButton = document.querySelector(".fa-times-circle")
+rules.addEventListener("click",function(){
+  rule1.classList.toggle("show");
+})
+closeButton.addEventListener("click",function(){
+  rule1.classList.toggle("show"); 
+})
+
+/**----------------Click New------------------- */
+let newButton = document.querySelector("#new");
+newButton.addEventListener("click",function(){
+  alert("Coming soon...💣")
+})
+
+/**----------------Click ?------------------- */
+let questionButton = document.querySelector(".fa-question-circle");
+questionButton.addEventListener("click",function(){
+  alert("Coming soon...💣")
+})
+
+
+/**----------------Click play------------------- */
+let playButton = document.querySelector("#play");
+playButton.addEventListener("click", function(){ 
+  enabled = true;
+    play(enabled);   
+})
+
+/**----------------Click reset------------------- */
+let resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function(){ 
+    localStorage.removeItem("winners"); 
+    location.reload(); 
+})
+
+
+/**-------------Modo oscuro boton-----------------*/
 let body = document.querySelector("body");
-
-//capturar el boton que es un input
 let boton = document.querySelector("input");
 
-//elemento de escucha al boton
-boton.addEventListener("click", function(){
-  
+boton.addEventListener("click", function(){   
     body.classList.toggle("dark");
     boton.classList.toggle("active")
-    if(document.body.classList.contains('dark')){ //cuando el cuerpo tiene la clase 'dark' actualmente
-      localStorage.setItem('darkMode', 'enabled'); //almacenar estos datos si el modo oscuro está activado
-  }else{
-      localStorage.setItem('darkMode', 'disabled'); //almacenar estos datos si el modo oscuro está desactivado
-  }
+    if(document.body.classList.contains('dark')){ 
+      localStorage.setItem('darkMode', 'enabled');
 
+    }else{
+      localStorage.setItem('darkMode', 'disabled'); 
+    }
 });
+
 if(localStorage.getItem('darkMode') == 'enabled'){
   body.classList.toggle('dark');
+  boton.setAttribute("checked","true");
 }
 
 
 /**-------------------Choice----------------- */
 
-/*
-*/
-
-function play(){
-  
-  
-
+function play(){  
   if(!timer) {
-    console.log(timer);
       timer = window.setInterval(function() { 
         myFunction();
       }, 1000); } 
   
-
-let option = document.querySelectorAll("#option");
-let youOption = document.querySelector("#you-choice");
-let machineOption = document.querySelector("#machine-choice");
-
-  option.forEach(function(choice){
-
-     
+  let option = document.querySelectorAll("#option");
+  let youOption = document.querySelector("#you-choice");
+  let machineOption = document.querySelector("#machine-choice");
+  
+  option.forEach(function(choice){  
     choice.addEventListener("click", function(){
-      
-
-      //console.log(choice.classList.value);
-   
+      if (enabled){
       /**---------------------------rock----------------------------- */
-      console.log(habilitado);
-      console.log(seconds)
-      if (habilitado){
-      if(choice.classList.value==="rock"){
-        let machineChoice = Math.floor( Math.random() * 3 + 1 );
-        youOption.src="img/rock.png";
-        if(machineChoice===1){
-          machineOption.src="img/rock.png";
-          empatados++;
-          console.log("Empataste, la maquina tambien escogió piedra")
-        }
+        if(choice.classList.value==="rock"){
+          let machineChoice = Math.floor( Math.random() * 3 + 1 );
+          youOption.src="img/rock.png";
+          if(machineChoice===1){
+            machineOption.src="img/rock.png";
+            tied++;
+          }
         else if(machineChoice===2){
           machineOption.src="img/paper.png";
-          perdidos++;
-          console.log("Perdiste, la maquina escogió papel");
+          lost++;
         }
         else{
           machineOption.src="img/scissors.png";
-          ganados++;
-          console.log("Ganaste, la maquina escogio tijeras")
+          won++;
         }
-        console.log(machineChoice)
       }
 
       /**-----------------------------paper---------------------------- */
@@ -157,60 +201,38 @@ let machineOption = document.querySelector("#machine-choice");
         youOption.src="img/paper.png";
         if(machineChoice===2){
           machineOption.src="img/paper.png";
-          empatados++;
-          console.log("Empataste, la maquina tambien escogió papel")
+          tied++;
         }
         else if(machineChoice===3){
           machineOption.src="img/scissors.png";
-          perdidos++;
-          console.log("Perdiste, la maquina escogió tijeras");
+          lost++;
         }
         else{
           machineOption.src="img/rock.png";
-          ganados++;
-          console.log("Ganaste, la maquina escogio piedra")
+          won++;
         }
-        console.log(machineChoice)
       }
       
-      /**---------------------------scissors------------------------------ */
+      /**---------------------------scissors--------------------------- */
       else if(choice.classList.value==="scissors"){
         let machineChoice = Math.floor( Math.random() * 3 + 1 );
         youOption.src="img/scissors.png";
         if(machineChoice===3){
           machineOption.src="img/scissors.png";
-          empatados++;
-          console.log("Empataste, la maquina tambien escogió tijeras")
-        }
+          tied++;
+          }
         else if(machineChoice===1){
           machineOption.src="img/rock.png";
-          perdidos++;
-          console.log("Perdiste, la maquina escogió piedra");
-        }
+          lost++;
+          }
         else{
           machineOption.src="img/paper.png";
-          ganados++;
-          console.log("Ganaste, la maquina escogio papel")
+          won++;
+          }
         }
-        console.log(machineChoice)
-      }
-
-      
-      
-      /*else if(choice.classList.value==="lizard")
-      youOption.src="img/lizard.png";
-      else if(choice.classList.value==="spock")
-      youOption.src="img/spock.png";*/
-     
-      }
-      
+      } 
     })
-    
-  
-  
-})
-
-  
+  })  
 }
 
 
